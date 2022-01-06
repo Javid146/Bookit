9ca22b4867e2e3c5334727c2214028e655fe0d68
Javid Mammadli <91811731+Javid146@users.noreply.github.com>
2022-01-05 23:16:20 -0500

    Update CancelReservation.java

--- src/Test/CancelReservation.java
+++ src/Test/CancelReservation.java
@@ -4,7 +4,7 @@ public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("I will try not to cancel my dinner reservation with mom!");
         
-        
+        //umid edirem ikisini de sevindirerem
         //mamaya can qurban. ele ataya da.
     }
 }
