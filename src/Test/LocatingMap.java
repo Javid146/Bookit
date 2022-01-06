d8fba23aa82eff0a2b58cf2ce81788f164f0c4c6
Javid146 <mammadli.javid15@gmail.com>
2022-01-05 23:42:44 -0500

    update LocatingMap.java

--- src/Test/LocatingMap.java
+++ src/Test/LocatingMap.java
@@ -3,6 +3,13 @@ package Test;
 public class LocatingMap {
     public static void main(String[] args) {
         System.out.println("I love learning.");
+
+        System.out.println("I love kindness.");
+
+
+
+
+
     }
 
 }
