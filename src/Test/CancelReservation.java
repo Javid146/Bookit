0bce054f6edbf4c11e042a7cbe4f34133997c509
Javid Mammadli <91811731+Javid146@users.noreply.github.com>
2022-01-05 23:21:27 -0500

    Update CancelReservation.java

--- src/Test/CancelReservation.java
+++ src/Test/CancelReservation.java
@@ -2,7 +2,6 @@ package Test;
 
 public class CancelReservation {
     public static void main(String[] args) {
-        System.out.println("I will try not to cancel my dinner reservation with mom!");
         
        System.out.println("Let's show love"); //umid edirem ikisini de sevindirerem
         //mamaya can qurban. ele ataya da.
