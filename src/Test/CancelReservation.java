bf3ee622fa16e7bde943f57b7cf34e9871a7d7a1
Javid146 <mammadli.javid15@gmail.com>
2022-01-05 22:29:37 -0500

    CancelReservation

---
+++ src/Test/CancelReservation.java
@@ -0,0 +1,7 @@
+package Test;
+
+public class CancelReservation {
+    public static void main(String[] args) {
+        System.out.println("I will try not to cancel my dinner reservation with mom!");
+    }
+}
