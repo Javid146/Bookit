a0d68ab26bf3e41749cf6e75bbbb7dbefb25728a
Javid Mammadli <91811731+Javid146@users.noreply.github.com>
2022-01-05 23:38:54 -0500

    Update LocatingMap.java

--- src/Test/LocatingMap.java
+++ src/Test/LocatingMap.java
@@ -3,6 +3,10 @@ package Test;
 public class LocatingMap {
     public static void main(String[] args) {
         System.out.println("I love learning.");
+        
+        System.out.println("I love WOMEN.");
+        
+        
     }
 
 }
